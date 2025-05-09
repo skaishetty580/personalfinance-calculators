a5b91837f9e57d39a3b082076f579d60867bfd2a
skaishetty580 <ksdhar@gmail.com>
2025-05-08 22:10:19 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-// app.js
 class FinanceApp {
     constructor() {
         this.calculators = {
@@ -16,6 +15,17 @@ class FinanceApp {
     init() {
         this.setupCalculatorCards();
         this.setupBackButton();
+        this.loadChartJS();
+    }
+
+    loadChartJS() {
+        // Load Chart.js if not already loaded
+        if (typeof Chart === 'undefined') {
+            const script = document.createElement('script');
+            script.src = 'https://cdn.jsdelivr.net/npm/chart.js';
+            script.onload = () => console.log('Chart.js loaded');
+            document.head.appendChild(script);
+        }
     }
 
     setupCalculatorCards() {
@@ -67,36 +77,84 @@ class FinanceApp {
         calculatorContent.innerHTML = '';
         
         // Load the appropriate calculator
-        if (calculatorType === 'mortgage') {
-            this.calculators.mortgage = new MortgageCalculator(calculatorContent);
-            this.currentCalculator = this.calculators.mortgage;
-        } 
-        else if (calculatorType === 'tax') {
-            if (!this.calculators.tax) {
-                this.calculators.tax = new TaxCalculator(calculatorContent);
-            } else {
-                calculatorContent.innerHTML = '';
-                this.calculators.tax.container = calculatorContent;
-                this.calculators.tax.renderForm();
-            }
-            this.currentCalculator = this.calculators.tax;
-        }
-        else {
-            calculatorContent.innerHTML = `
-                <div style="text-align: center; padding: 40px 0;">
-                    <i class="fas fa-cogs" style="font-size: 50px; color: var(--gray-color); margin-bottom: 20px;"></i>
-                    <h3>Calculator Coming Soon</h3>
-                    <p>This calculator is not yet implemented in this demo.</p>
-                    <button class="button back-button" style="margin-top: 20px;">
-                        <i class="fas fa-arrow-left"></i> Back to Home
-                    </button>
-                </div>
-            `;
-            
-            // Add event listener to back button
-            calculatorContent.querySelector('.back-button').addEventListener('click', () => {
-                this.showHomePage();
-            });
+        switch(calculatorType) {
+            case 'mortgage':
+                if (!this.calculators.mortgage) {
+                    this.calculators.mortgage = new MortgageCalculator(calculatorContent);
+                } else {
+                    calculatorContent.innerHTML = '';
+                    this.calculators.mortgage.container = calculatorContent;
+                    this.calculators.mortgage.renderForm();
+                }
+                this.currentCalculator = this.calculators.mortgage;
+                break;
+                
+            case 'investment':
+                if (!this.calculators.investment) {
+                    this.calculators.investment = new InvestmentCalculator(calculatorContent);
+                } else {
+                    calculatorContent.innerHTML = '';
+                    this.calculators.investment.container = calculatorContent;
+                    this.calculators.investment.renderForm();
+                }
+                this.currentCalculator = this.calculators.investment;
+                break;
+                
+            case 'debt':
+                if (!this.calculators.debt) {
+                    this.calculators.debt = new DebtCalculator(calculatorContent);
+                } else {
+                    calculatorContent.innerHTML = '';
+                    this.calculators.debt.container = calculatorContent;
+                    this.calculators.debt.renderForm();
+                }
+                this.currentCalculator = this.calculators.debt;
+                break;
+                
+            case 'retirement':
+                if (!this.calculators.retirement) {
+                    this.calculators.retirement = new RetirementCalculator(calculatorContent);
+                } else {
+                    calculatorContent.innerHTML = '';
+                    this.calculators.retirement.container = calculatorContent;
+                    this.calculators.retirement.renderForm();
+                }
+                this.currentCalculator = this.calculators.retirement;
+                break;
+                
+            case 'budget':
+                if (!this.calculators.budget) {
+                    this.calculators.budget = new BudgetCalculator(calculatorContent);
+                } else {
+                    calculatorContent.innerHTML = '';
+                    this.calculators.budget.container = calculatorContent;
+                    this.calculators.budget.renderForm();
+                }
+                this.currentCalculator = this.calculators.budget;
+                break;
+                
+            case 'tax':
+                if (!this.calculators.tax) {
+                    this.calculators.tax = new TaxCalculator(calculatorContent);
+                } else {
+                    calculatorContent.innerHTML = '';
+                    this.calculators.tax.container = calculatorContent;
+                    this.calculators.tax.renderForm();
+                }
+                this.currentCalculator = this.calculators.tax;
+                break;
+                
+            default:
+                calculatorContent.innerHTML = `
+                    <div style="text-align: center; padding: 40px 0;">
+                        <i class="fas fa-cogs" style="font-size: 50px; color: var(--gray-color); margin-bottom: 20px;"></i>
+                        <h3>Calculator Coming Soon</h3>
+                        <p>This calculator is not yet implemented in this demo.</p>
+                        <button class="button back-button" style="margin-top: 20px;">
+                            <i class="fas fa-arrow-left"></i> Back to Home
+                        </button>
+                    </div>
+                `;
         }
         
         // Scroll to top
